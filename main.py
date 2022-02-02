df01ffc13cc5f5887ae63c955ca7ae74a479ead3
DonGitSchool <92173334+DonGitSchool@users.noreply.github.com>
2022-02-01 21:05:42 -0700

    changed it so it only uses collusion, simple because ive had my fun

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def on_button_pressed_a():
+def on_button_pressed_b():
     music.set_volume(pins.map(pins.analog_read_pin(AnalogPin.P3), 800, 1020, 50, 255))
-input.on_button_pressed(Button.A, on_button_pressed_a)
+input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_forever():
     if pins.digital_read_pin(DigitalPin.P0) == 0:
@@ -11,6 +11,6 @@ def on_forever():
                         . . . # .
                         . . . . #
         """)
+        music.start_melody(music.built_in_melody(Melodies.FUNK), MelodyOptions.ONCE)
         pins.digital_write_pin(DigitalPin.P0, 1)
-        music.start_melody(music.built_in_melody(Melodies.NYAN), MelodyOptions.ONCE)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,22 +1,13 @@
-input.onButtonPressed(Button.B, function () {
-    music.setVolume(pins.map(
-    pins.analogReadPin(AnalogPin.P3),
-    800,
-    1020,
-    50,
-    255
-    ))
-})
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P0) == 0) {
         basic.showLeds(`
-            # . . . .
-            . # . . .
-            . . # . .
-            . . . # .
-            . . . . #
+            # . # . #
+            . # . # .
+            . . . . .
+            . # . # .
+            # . # . #
             `)
-        music.startMelody(music.builtInMelody(Melodies.Funk), MelodyOptions.Once)
+        music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
         pins.digitalWritePin(DigitalPin.P0, 1)
     }
 })
